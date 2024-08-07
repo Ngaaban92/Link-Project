1b954ec526807a3df293692ba4144004801fec82
Nga Abane <ngaaban92@gmail.com>
2024-08-07 17:12:32 +0400

    Project initialized

---
+++ workspace.java
@@ -0,0 +1,5 @@
+This project is created by us to manage the end-to-end development of Link mobile and Desktop
+# Date: 07th August, 2024
+# Project Owner: Daddy D
+# Sub-project Owner : Darren Franck
+
